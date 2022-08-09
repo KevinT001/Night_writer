1b8a9bf97ba4a50ddfae16d6a32808b129fab831
Kevin Ta <kvnta@yahoo.com>
2022-08-08 23:59:16 -0600

    Test: translate_to_braille. Single line

--- spec/translateable_spec.rb
+++ spec/translateable_spec.rb
@@ -2,12 +2,20 @@ require './lib/translateable.rb'
 
 RSpec.describe Translateable do 
 
+  describe "translate_to_braille" do 
+    it "translates a single english line to braille" do 
+      english_text = "hello word"
+      expected = "0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0..."
+      expect(Translateable::translate_to_braille(english_text)).to eq(expected)
+    end
+  end
   describe "translate_to_english" do
     it "translates single line of braille" do
       braille_text = "0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0..."
 
       expect(Translateable::translate_to_english(braille_text)).to eq("hello world")
     end
+  end
 
     
 end
