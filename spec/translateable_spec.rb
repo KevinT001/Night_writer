cef9bcf6312bd56d49671d1f0c43e5184c215e31
Kevin Ta <kvnta@yahoo.com>
2022-08-08 23:48:50 -0600

    Test: translate_to_english

--- spec/translateable_spec.rb
+++ spec/translateable_spec.rb
@@ -2,5 +2,12 @@ require './lib/translateable.rb'
 
 RSpec.describe Translateable do 
 
-  it 'can translate english alphabet characters or text to braille characters in nested array'
+  describe "translate_to_english" do
+    it "translates single line of braille" do
+      braille_text = "0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0..."
+
+      expect(Translateable::translate_to_english(braille_text)).to eq("hello world")
+    end
+
+    
 end
