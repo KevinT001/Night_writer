41fc1dd2c72065de2fa88e0f0030a03dba283910
Kevin Ta <kvnta@yahoo.com>
2022-08-08 23:53:38 -0600

    Set up NightWriter before block with dummy read and write files. Test exists

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -2,7 +2,9 @@ require './lib/night_writer'
 
 RSpec.describe NightWriter do 
   before :each do 
-    @night_writer = NightWriter.new(ARGV[0], ARGV[1])
+    @read_file = "nw_read_file_dummy"
+    @write_file = "nw_write_file_dummy"
+    @night_writer = NightWriter.new(@read_file, @write_file)
   end
 
   describe 'iteration 1' do 
@@ -10,20 +12,5 @@ RSpec.describe NightWriter do
     it 'exists' do 
       expect(@night_writer).to be_a(NightWriter)
     end
-
-    it 'has has attributes' do 
-      expect(@night_writer.read_file).to eq(ARGV[0])
-      expect(@night_writer.write_file).to eq(ARGV[1])
-    end
-
-    xit 'dfdsf' do 
-      expect(@night_writer.input_file_character_count).to eq("3")
-    end
-
-    it 'can count the number of characters in the read_file' do 
-      expect(@night_writer.read_file_char_count)
-    end
-
   end
-
 end
